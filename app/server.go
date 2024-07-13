3cf85d28c61ea6fd592a61a74d32ffe5e840591d
Mayank <mayank525.work@gmail.com>
2024-07-14 00:44:53 +0530

    3rd pass stage

--- app/server.go
+++ app/server.go
@@ -5,6 +5,7 @@ import (
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -18,12 +19,20 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+
 	
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-
+	
+	req := make([]byte, 1024)
+	conn.Read(req)
+	if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		conn.Close()
+		return
+	}
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
