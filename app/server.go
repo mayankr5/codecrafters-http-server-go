df577068ce67d6e2386d62f207f5fca2a412a9e5
Mayank <mayank525.work@gmail.com>
2024-07-14 17:39:40 +0530

    4th stage pass

--- app/server.go
+++ app/server.go
@@ -26,13 +26,24 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	
+
 	req := make([]byte, 1024)
-	conn.Read(req)
-	if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		conn.Close()
+	n, err := conn.Read(req)
+	if err != nil {
+		fmt.Println("Failed to read the request:", err)
 		return
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
+	request := string(req[:n])
+	path := strings.Split(request, " ")[1]
+
+	if path == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if strings.Split(path, "/")[1] == "echo" {
+		message := strings.Split(path, "/")[2]
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+
 }
